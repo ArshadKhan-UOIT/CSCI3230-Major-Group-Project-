e775b6325251e0fefb36eb9f355ad406ca56bf24
Ryan Christopher <ryan.christopher1@ontariotechu.net>
2022-04-21 20:48:37 -0400

    Initial Project Creation

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+// const fs = require('fs');
+// const axios = require('axios');
+const app = express();
+
+app.use(express.static('public'));
+
+app.get("/", function(request, response){
+  response.sendFile( __dirname +'/public/index.html');
+});
+
+app.set('port',process.env.PORT ||4500);
+app.listen(app.get('port'),function(){
+  console.log(`Nodejs Server is up and listing to port ${app.get('port')}`);
+});
