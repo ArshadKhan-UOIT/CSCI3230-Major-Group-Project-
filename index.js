671c430f7a3c716fbb9667ac696793e6ec690f6b
Ryan Christopher <ryan.christopher1@ontariotechu.net>
2022-04-21 21:02:23 -0400

    Added Axios to Dependencies

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const express = require('express');
-// const fs = require('fs');
-// const axios = require('axios');
+const axios = require('axios');
 const app = express();
 
 app.use(express.static('public'));
