ad7419bb7c0ad871851b900d87a33f400df0dd23
Ryan Christopher <ryan.christopher1@ontariotechu.net>
2022-04-22 02:52:52 -0400

    Calls to FakeStoreApi Functional in products.js

--- index.js
+++ index.js
@@ -8,6 +8,27 @@ app.get("/", function(request, response){
   response.sendFile( __dirname +'/public/index.html');
 });
 
+app.get("/products", function(request, response){
+  console.log("https://fakestoreapi.com/products");
+
+  let url = "";
+  if (request.query.type == "all") {
+    url = "";
+  }
+  else if (request.query.type == "id") {
+    url = "/" + request.query.query;
+  }
+  else if (request.query.type == "category") {
+    url = "/" + request.query.type + "/" + request.query.query ;
+  }
+
+  console.log("calling: " + url);
+
+  axios.get("https://fakestoreapi.com/products" + url)
+    .then(res => response.send(res.data));
+});
+
+
 app.set('port',process.env.PORT ||4500);
 app.listen(app.get('port'),function(){
   console.log(`Nodejs Server is up and listing to port ${app.get('port')}`);
