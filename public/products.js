56ab4425908a7fd64158ae4719f1d9d52862c977
ArshadKhan-UOIT <arshad.khan@ontariotechu.net>
2022-04-25 15:33:24 -0400

    removing unnessessary files

--- public/products.js
+++ public/products.js
@@ -3,48 +3,48 @@ function getAllProducts() {
 }
 
 function getProductsByID(query) {
-  console.log("by id");
+  // console.log("by id");
   let productList = [];
   productList.push(productAPICall("id", query));
 
-  console.log(productList);
+  // console.log(productList);
   return productList;
 }
 
 function getProductsByTitle(query) {
-  console.log("by title");
+  // console.log("by title");
   let productList = [];
   productAPICall("all", query).forEach((item, i) => {
     if (item["title"].includes(query)) {
       productList.push(item);
     }
   });
-  console.log(productList);
+  // console.log(productList);
   return productList;
 }
 
 function getProductsByCategory(query) {
-  console.log("by category");
+  // console.log("by category");
   let productList = productAPICall("category", query);
 
-  console.log(productList);
+  // console.log(productList);
   return productList;
 }
 
 function getProductsByPrice(query) {
-  console.log("by price");
+  // console.log("by price");
   let productList = [];
   productAPICall("all", query).forEach((item, i) => {
     if (item["price"] >= query[0] && item["price"] <= query[1]) {
       productList.push(item);
     }
   });
-  console.log(productList);
+  // console.log(productList);
   return productList;
 }
 
 function getProductsByRating(query) {
-  console.log("by rating");
+  // console.log("by rating");
 
   let productList = [];
   // sort by query
@@ -54,7 +54,7 @@ function getProductsByRating(query) {
       productList.push(item);
     }
   });
-  console.log(productList);
+  // console.log(productList);
   return productList;
 }
 
@@ -70,13 +70,13 @@ function productAPICall(type, query) {
     success: function(data) {
       productsList = data;
     },
-    failure: function(data) {console.log("failure");}
+    failure: function(data) {/*console.log("failure");*/}
   });
   return productsList;
 }
 
 $(document).ready(function() {
-  console.log("start");
+  // console.log("start");
 
   let list = $("#all");
   let products = [];
